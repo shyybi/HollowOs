1c3c68fce08236cd5fc150d8d17bf91689fcb51c
shyybi <charlotte.rod@outlook.com>
2025-09-07 17:22:14 +0200

    Refactor bootloader and kernel code for clarity and organization

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,8 +1,20 @@
 [BITS 32]
 
-global _start
+load32:
+  mov ax, DATA_SEG
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+  mov ebp, 0x00200000
+	mov esp, ebp
+  
+	; fast enable a20
+  in al, 0x92
+  or al, 2
+  out 0x92, al
+  
+	jmp $
 
-;_start: 
-
-
-times 512-($- $$) db 0 ; filling void to 512o
+	times 512-($- $$) db 0 ;
--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,23 @@
 #include "kernel.h"
 
 void kernel_main(void) {
+	const char *str = "HollowOs";
+	char *vidptr = (char*)0xb8000; /* video mem */
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	/* clearing screen */
+	while(j < 80 * 25 * 2) {
+        vidptr[j] = ' '; 				/*Blank chara ' ' */
+        vidptr[j+1] = 0x07; 		/* Light grey */
+        j = j + 2;
+    }
+
+  while(str[j] != '\0') {
+    vidptr[i] = str[j];
+  	vidptr[i+1] = 0x07;
+    ++j;
+    i = i + 2;
+  }
+  return;
 }
